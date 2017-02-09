82b76e18aa7a526ed6d67b9182abc860cfcac917
Artem Golovchenko <golovchenko84@mail.ru>
2017-02-09 17:42:16 +0200

    add class Android and second instrument with qualifier (mega_balalayka)

--- src/main/java/com/springinaction/springidol/Balalayka.java
+++ src/main/java/com/springinaction/springidol/Balalayka.java
@@ -1,7 +1,18 @@
 package com.springinaction.springidol;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by головченко on 09.02.2017.
  */
-public class Balalayka {
+
+
+public class Balalayka implements Instrument{
+
+
+    @Override
+    public void play() {
+        System.out.println("Brin Brin Brin");
+    }
 }
--- src/main/java/ua/golovchenko/artem/humans/Android.java
+++ src/main/java/ua/golovchenko/artem/humans/Android.java
@@ -2,6 +2,7 @@ package ua.golovchenko.artem.humans;
 
 import com.springinaction.springidol.Instrument;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import ua.golovchenko.artem.humans.clothes.Clothes;
 import ua.golovchenko.artem.humans.clothes.jackets.LeatherJacket;
 
@@ -11,18 +12,27 @@ import ua.golovchenko.artem.humans.clothes.jackets.LeatherJacket;
 
 public class Android {
 
-
+    @Autowired
     Instrument instrument;
+
+    @Autowired
+    @Qualifier("mega_balalayka")
+    Instrument instrument2;
 
     public Android(){}
 
-    @Autowired
+
     public Android(Instrument instrument) {
         this.instrument = instrument;
     }
 
+    public Android(Instrument instrument, Instrument instrument2) {
+        this.instrument = instrument;
+        this.instrument2 = instrument2;
+    }
 
     public void iPlayOn(){
         instrument.play();
+        instrument2.play();
     }
 }
